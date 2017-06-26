76a7c0008800ce68d074f37746f71858f1c7725a
hemanthk97 <hemanthk97@yahoo.com>
2017-06-27 02:36:25 +0530

    Efficient way of findings non repeating numbers using python Counter

--- code_arena/Triangles_problem/submitted_ans.py
+++ code_arena/Triangles_problem/submitted_ans.py
@@ -1,15 +1,3 @@
-try:
-   N = 5
-   a = [[7, 6, 5],[5, 7, 6],[8, 2, 9],[2, 3, 4],[2, 4, 3]]
-   if N <= 0 and N >= 10**5:
-       raise valueError(0)
-   lis = []
-   for i in range(N):
-       lis.append(sum(a[i]))
-   unique = 0
-   for i in lis:
-       if lis.count(i) == 1:
-          unique = unique + 1
-   print(unique)
-except:
-    print(0)
+from collections import Counter
+
+print(Counter([" ".join(sorted(raw_input().rstrip().split(" "))) for i in range(input())]).values().count(1))
